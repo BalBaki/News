1a090f2b60756a4b492bd78735e9d76620774c5a
Baki <bakibal1994@gmail.com>
2023-12-30 21:42:31 +0300

    Changed Auth Mw Logic

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,40 +2,28 @@ const jwt = require('jsonwebtoken');
 const { createAccessTokenCookies, clearTokenCookies } = require('../utils');
 
 const authMiddleware = (request, response, next) => {
-    const { accessToken, refreshToken } = request.cookies;
-
-    if (!accessToken) {
-        clearTokenCookies(response);
-
-        return response.json({ auth: false, error: 'No Access Token. Access Denied' });
-    }
-
     try {
-        const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET_KEY);
+        const { accessToken, refreshToken } = request.cookies;
 
-        request.user = { id: decodedAccessToken.id, email: decodedAccessToken.email };
+        if (!accessToken || !refreshToken) throw new Error();
 
-        next();
-    } catch (error) {
-        if (!refreshToken) {
-            clearTokenCookies(response);
-
-            return response.json({ auth: false, error: 'No Refresh Token. Access Denied' });
-        }
+        const decodedRefreshToken = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET_KEY);
 
         try {
-            const decodedRefreshToken = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET_KEY);
+            const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET_KEY);
 
+            request.user = { id: decodedAccessToken.id, email: decodedAccessToken.email };
+        } catch (error) {
             request.user = { id: decodedRefreshToken.id, email: decodedRefreshToken.email };
 
             createAccessTokenCookies(response, { id: decodedRefreshToken.id, email: decodedRefreshToken.email });
-
-            next();
-        } catch (error) {
-            clearTokenCookies(response);
-
-            return response.send({ auth: 'false', error: 'Not Valid Refresh Token' });
         }
+
+        next();
+    } catch (error) {
+        clearTokenCookies(response);
+
+        return response.send({ verify: 'false', error: 'Access Denied' });
     }
 };
 
